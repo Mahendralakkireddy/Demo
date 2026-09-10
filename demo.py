d21d6f6d4a6a90bd8666031883eb0d37cad0e97c
Mahendralakkireddy <lakkireddymahendra11@gmail.com>
2026-09-10 21:26:00 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3986,7 +3986,7 @@ def _home_page():
       .st-key-top_home,
       .st-key-top_docs {
           position: absolute !important;
-          top: 15px !important;
+          top: 48px !important;
           z-index: 10000 !important;
           margin: 0 !important;
           padding: 0 !important;
