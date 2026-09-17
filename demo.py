5c75ae3124a20e68c082a5e51ff0a0c7cecf333f
Mahendralakkireddy <lakkireddymahendra11@gmail.com>
2026-09-17 12:43:37 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1026,9 +1026,9 @@ def call_cortex_analyst(prompt: str) -> Dict[str, Any]:
             "content": [{"type": "text", "text": prompt}],
         }],
         "semantic_models": [
-       
-            {"semantic_model_file": SUPPLY_CHAIN_YAML_STAGE_PATH}
-            
+            {"semantic_model_file": INVENTORY_YAML_STAGE_PATH},
+            {"semantic_model_file": SALES_YAML_STAGE_PATH},
+            {"semantic_model_file": SUPPLY_CHAIN_YAML_STAGE_PATH},
         ],
         "stream": False,
     }
